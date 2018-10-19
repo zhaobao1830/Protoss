f1f53367a6970bb9efa48b18b74cc8dc95c3217a
zhaobao1830 <373098777@qq.com>
2018-10-19 14:27:29 +0800

    主题页

---
+++ pages/theme/theme.js
@@ -0,0 +1,33 @@
+// pages/theme/theme.js
+
+import { Theme } from 'theme-model.js';
+var theme = new Theme(); //实例化  主题列表对象
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    let id = options.id;
+    let name = options.name;
+    this.data.id = id;
+    this.data.name = name;
+    this._loadData();
+  },
+
+  _loadData:function(){
+    theme.getProductsData(this.data.id,(data)=>{
+      this.setData({
+        themeInfo:data
+      });
+    });
+  }
+})
