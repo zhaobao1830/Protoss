3cbe1916a8613643a1a4bf890aadaf2662285e5e
zhaobao1830 <x5219438>
2018-10-28 17:09:04 +0800

    分类页完成

--- pages/category/category.js
+++ pages/category/category.js
@@ -1,66 +1,91 @@
 // pages/category/category.js
+
+import {Category} from 'category-model'
+var category = new Category();
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    currentMenuIndex: 0,
+    loadedData: {}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this._loadData();
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+  _loadData:function () {
+    category.getCategoryType((categoryData)=>{
+      this.setData({
+        categoryTypeArr: categoryData
+      });
 
+      category.getProductsByCategory(
+        categoryData[0].id,(data)=>{
+          var dataObj = {
+            procucts: data,
+            topImgUrl: categoryData[0].img.url,
+            title: categoryData[0].name
+          };
+
+          this.setData({
+            categoryProducts: dataObj
+          })
+          this.data.loadedData[0] = dataObj
+
+        })
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+  changeCategory:function (event) {
+    var index = category.getDataSet(event, 'index'),
+      id = category.getDataSet(event, 'id')//获取data-set
 
+    this.setData({
+      currentMenuIndex: index
+    });
+
+    if(!this.isLoadedData(index)) {
+      category.getProductsByCategory(
+        id, (data)=>{
+          var dataObj = {
+            procucts: data,
+            topImgUrl:this.data.categoryTypeArr[index].img.url,
+            title: this.data.categoryTypeArr[index].name
+          };
+
+          this.setData({
+            categoryProducts: dataObj
+          })
+
+          this.data.loadedData[index] = dataObj
+        }
+      )
+    } else {
+      this.setData({
+        categoryProducts: this.data.loadedData[index]
+      })
+    }
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  // 判断当前分类下的商品数据是否已经被加载过
+  isLoadedData: function (index) {
+    if (this.data.loadedData[index]) {
+      return true;
+    }
+    return false;
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onProductsItemTap:function (event) {
+    var id = category.getDataSet(event, 'id');
+    wx.navigateTo({
+      url: '../product/product?id=' + id
+    })
   }
 })
