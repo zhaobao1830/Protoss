3afe12ddba2a33ea2ab57616c2ec32ca765bc862
zhaobao1830 <373098777@qq.com>
2018-11-12 17:12:50 +0800

    新建支付结果

---
+++ pages/pay-result/pay-result.js
@@ -0,0 +1,27 @@
+// pages/pay-result/pay-result.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.setData({
+      payResult: options.flag,
+      id: options.id,
+      from: options.from
+    });
+  },
+
+  viewOrder: function () {
+    wx.navigateBack({
+      data: 1
+    })
+  }
+})
